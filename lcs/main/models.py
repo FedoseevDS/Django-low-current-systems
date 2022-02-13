91b467a22bbdb6077ecd688cd67a32a5607d507c
Dmitriy Fedoseev <dmsr.fedoseev@gmail.com>
2022-02-13 21:16:16 +0300

    Added the model, footer to the main page, Connected the admin panel.

--- lcs/main/models.py
+++ lcs/main/models.py
@@ -4,8 +4,15 @@ from django.db import models
 # Create your models here.
 
 class Main_page(models.Model):
+    title = models.CharField(max_length=150, null=True)
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to='photos/%y/%m/%d/')
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.content # отображает на сайте текст базы данных
+
+    def get_absolute_url(self):
+        return reverse('index_page', kwargs={'index_page_id': self.pk})
